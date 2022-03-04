6834ec496fc899856e71cc84cd2d04d79a70df3a
MangueD <99347781+MangueD@users.noreply.github.com>
2022-03-04 15:07:36 +0100

    Create PopStackToDoubleRegister.h

---
+++ src/cpu/instruction/opcode/pop/PopStackToDoubleRegister.h
@@ -0,0 +1,18 @@
+#ifndef __POP_STACK_TO_DOUBLE_REGISTER_H__
+#define __POP_STACK_TO_DOUBLE_REGISTER_H__
+#include "../Instruction.h"
+
+class PopStackToDoubleRegister : public Instruction{
+    private:
+        DoubleRegisterName doubleRegName;
+    public:
+        PopStackToDoubleRegister(DoubleRegisterName);
+
+        void execute(Memory&, Registers&) override;
+        unsigned int getSize() override;
+        unsigned int getTiming() override;
+        void setParameters(const Memory&, unsigned short) override;
+};
+
+
+#endif
