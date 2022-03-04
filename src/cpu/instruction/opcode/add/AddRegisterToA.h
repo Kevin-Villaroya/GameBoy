b05dbcb3e96370c852755f82178c7c1bb0092902
MangueD <99347781+MangueD@users.noreply.github.com>
2022-03-04 15:18:35 +0100

    Create AddRegisterToA.h

---
+++ src/cpu/instruction/opcode/add/AddRegisterToA.h
@@ -0,0 +1,18 @@
+#ifndef __ADD_REGISTER_TO_A_H__
+#define __ADD_REGISTER_TO_A_H__
+#include "../Instruction.h"
+
+class AddRegisterToA : public Instruction{
+    private:
+        RegisterName registerName;
+
+    public:
+        AddRegisterToA(RegisterName);
+
+        void execute(Memory& ram, Registers& registers) override;
+        unsigned int getSize() override;
+        unsigned int getTiming() override;
+        void setParameters(const Memory&, unsigned short) override;
+};
+
+#endif 
