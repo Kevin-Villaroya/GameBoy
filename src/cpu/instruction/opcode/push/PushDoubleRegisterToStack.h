aed5e93ef62cb676160cead0b9c829b84163fe67
MangueD <99347781+MangueD@users.noreply.github.com>
2022-03-04 15:09:26 +0100

    Create PushDoubleRegisterToStack.h

---
+++ src/cpu/instruction/opcode/push/PushDoubleRegisterToStack.h
@@ -0,0 +1,18 @@
+#ifndef __PUSH_DOUBLE_REGISTER_TO_STACK_H__
+#define __PUSH_DOUBLE_REGISTER_TO_STACK_H__
+#include "../Instruction.h"
+
+class PushDoubleRegisterToStack : public Instruction{
+    private:
+        DoubleRegisterName doubleRegName;
+    public:
+        PushDoubleRegisterToStack(DoubleRegisterName);
+
+        void execute(Memory&, Registers&) override;
+        unsigned int getSize() override;
+        unsigned int getTiming() override;
+        void setParameters(const Memory&, unsigned short) override;
+};
+
+
+#endif
