d64e84bbb751138fb2a61ee43ad6f1f34b37bfc8
MangueD <99347781+MangueD@users.noreply.github.com>
2022-03-06 16:16:03 +0100

    Create SubCarryDoubleRegisterToA.cpp

---
+++ src/cpu/instruction/opcode/sub/SubCarryDoubleRegisterToA.cpp
@@ -0,0 +1,36 @@
+#include "SubCarryDoubleRegisterToA.h"
+
+SubCarryDoubleRegisterToA::SubCarryDoubleRegisterToA(DoubleRegisterName d):doubleRegName(d){}
+
+void SubCarryDoubleRegisterToA::execute(Memory& ram, Registers& registers){
+    unsigned char regValue = registers.getDoubleRegister(this->doubleRegName);
+    unsigned char aValue = registers.getA()+(registers.isFlagC()?1:0);
+
+    if(regValue-aValue == 0)
+        registers.setFlagZ(1);
+    else
+        registers.setFlagZ(0);
+    
+    if((aValue&0b00001111) < (regValue&0b00001111))
+        registers.setFlagH(0);
+    else
+        registers.setFlagH(1);
+
+    if(aValue < regValue)
+        registers.setFlagC(0);
+    else
+        registers.setFlagC(1);
+    
+    registers.setFlagN(1);
+}
+
+unsigned int SubCarryDoubleRegisterToA::getSize(){
+    return 1;
+}
+
+unsigned int SubCarryDoubleRegisterToA::getTiming(){
+    return 8;
+}
+
+void SubCarryDoubleRegisterToA::setParameters(const Memory&, unsigned short v){
+}
