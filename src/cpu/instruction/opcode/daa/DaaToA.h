2f1fb3f64e3cb2108d97bc1c65c7129a3a720b95
MangueD <99347781+MangueD@users.noreply.github.com>
2022-03-13 21:20:07 +0100

    Add files via upload

---
+++ src/cpu/instruction/opcode/daa/DaaToA.h
@@ -0,0 +1,18 @@
+#ifndef __DAA_TO_A_H__
+#define __DAA_TO_A_H__
+#include "../Instruction.h"
+
+class DaaToA : public Instruction{
+    private:
+        RegisterName registerName;
+    public:
+        DaaToA();
+
+        void execute(Memory&, Registers&) override;
+        unsigned int getSize() override;
+        unsigned int getTiming() override;
+        void setParameters(const Memory&, unsigned short) override;
+};
+
+
+#endif
