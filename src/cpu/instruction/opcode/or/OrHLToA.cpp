aeffc632404c6a27ffa48cdd9f6d4fa88e269925
MangueD <99347781+MangueD@users.noreply.github.com>
2022-03-13 18:09:05 +0100

    Add files via upload

---
+++ src/cpu/instruction/opcode/or/OrHLToA.cpp
@@ -0,0 +1,29 @@
+#include "OrHLToA.h"
+
+OrHLToA::OrHLToA(){}
+
+void OrHLToA::execute(Memory& ram, Registers& registers){
+    unsigned short hlValue = registers.getDoubleRegister(DoubleRegisterName::HL);
+    unsigned char toOrValue = ram.get(hlValue);
+    unsigned char aVal = registers.getRegister(RegisterName::A);
+    registers.setA(toOrValue&aVal);
+
+    if(toOrValue|aVal == 0)
+        registers.setFlagZ(1);
+    else 
+        registers.setFlagZ(0);
+    registers.setFlagN(1);
+    registers.setFlagC(0);
+    registers.setFlagH(1);
+}
+
+unsigned int OrHLToA::getSize(){
+    return 1;
+}
+
+unsigned int OrHLToA::getTiming(){
+    return 8;
+}
+
+void OrHLToA::setParameters(const Memory&, unsigned short){
+}
---
+++ src/cpu/instruction/opcode/or/OrHLToA.h
@@ -0,0 +1,18 @@
+#ifndef __OR_HL_TO_A_H__
+#define __OR_HL_TO_A_H__
+#include "../Instruction.h"
+
+class OrHLToA : public Instruction{
+    private:
+        DoubleRegisterName doubleRegisterName;
+    public:
+        OrHLToA();
+
+        void execute(Memory&, Registers&) override;
+        unsigned int getSize() override;
+        unsigned int getTiming() override;
+        void setParameters(const Memory&, unsigned short) override;
+};
+
+
+#endif
