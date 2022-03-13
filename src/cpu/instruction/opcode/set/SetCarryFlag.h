5bbf715924867a657535187001da9b6fe7789a72
MangueD <99347781+MangueD@users.noreply.github.com>
2022-03-13 21:22:55 +0100

    Add files via upload

---
+++ src/cpu/instruction/opcode/set/SetCarryFlag.h
@@ -0,0 +1,18 @@
+#ifndef __SET_CARRY_FLAG_H__
+#define __SET_CARRY_FLAG_H__
+#include "../Instruction.h"
+
+class SetCarryFlag : public Instruction{
+    private:
+        RegisterName registerName;
+    public:
+        SetCarryFlag();
+
+        void execute(Memory&, Registers&) override;
+        unsigned int getSize() override;
+        unsigned int getTiming() override;
+        void setParameters(const Memory&, unsigned short) override;
+};
+
+
+#endif
