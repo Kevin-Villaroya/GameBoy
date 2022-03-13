d7d3327b8e847f1a2fd49ec6096a6b9879c2e6d7
MangueD <99347781+MangueD@users.noreply.github.com>
2022-03-13 18:08:20 +0100

    Add files via upload

---
+++ src/cpu/instruction/opcode/cp/CpHLToA.cpp
@@ -0,0 +1,35 @@
+#include "CpHLToA.h"
+
+CpHLToA::CpHLToA(){}
+
+void CpHLToA::execute(Memory& ram, Registers& registers){
+    unsigned short hlValue = registers.getDoubleRegister(DoubleRegisterName::HL);
+    unsigned char toCpValue = ram.get(hlValue);
+    unsigned char aVal = registers.getRegister(RegisterName::A);
+
+    if(toCpValue == aVal)
+        registers.setFlagZ(1);
+    else 
+        registers.setFlagZ(0);
+    registers.setFlagN(1);
+    if((aVal&0b00001111) < (toCpValue&0b00001111))
+        registers.setFlagH(1);
+    else
+        registers.setFlagH(0);
+
+    if(aVal < toCpValue)
+        registers.setFlagC(1);
+    else
+        registers.setFlagC(0);
+}
+
+unsigned int CpHLToA::getSize(){
+    return 1;
+}
+
+unsigned int CpHLToA::getTiming(){
+    return 8;
+}
+
+void CpHLToA::setParameters(const Memory&, unsigned short){
+}
