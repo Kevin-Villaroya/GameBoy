7344d9b54214a93669a7ba0e31415fe5a25a9bce
MangueD <99347781+MangueD@users.noreply.github.com>
2022-03-30 20:40:00 +0200

    Add files via upload

---
+++ src/cpu/instruction/opcode/sl/ShiftLeftHL.h
@@ -0,0 +1,19 @@
+#ifndef __SHIFT_LEFT_HL_H__
+#define __SHIFT_LEFT_HL_H__
+#include "../Instruction.h"
+
+class ShiftLeftHL : public Instruction{
+    private:
+        RegisterName registerName;
+    public:
+        ShiftLeftHL();
+
+        void execute(Memory&, Registers&) override;
+        unsigned int getSize() override;
+        unsigned int getTiming() override;
+        void setParameters(const Memory&, unsigned short) override;
+        std::string toString() override;
+};
+
+
+#endif
---
+++ src/cpu/instruction/opcode/sl/ShiftLeftRegister.cpp
@@ -0,0 +1,30 @@
+#include "ShiftLeftRegister.h"
+
+ShiftLeftRegister::ShiftLeftRegister(RegisterName r):registerName(r){}
+
+void ShiftLeftRegister::execute(Memory& ram, Registers& registers){
+    unsigned char aValue = registers.getRegister(this->registerName);
+	bool lastBit = aValue & 0b10000000;
+	
+	aValue <<= 1;
+    aValue == 0 ? registers.setFlagZ(0) : registers.setFlagZ(1);
+    registers.setFlagC(lastBit);
+    registers.setFlagH(0);
+    registers.setFlagN(0);
+    registers.setRegister(registerName, aValue);
+}
+
+unsigned int ShiftLeftRegister::getSize(){
+    return 2;
+}
+
+unsigned int ShiftLeftRegister::getTiming(){
+    return 8;
+}
+
+void ShiftLeftRegister::setParameters(const Memory&, unsigned short v){
+}
+
+std::string ShiftLeftRegister::toString(){
+    return "SLA " + Registers::getRegisterName(this->registerName);
+}
