7a80eb2b57b193a25f1114a75f6be120a8bca2f2
MangueD <99347781+MangueD@users.noreply.github.com>
2022-03-30 20:19:15 +0200

    Add files via upload

---
+++ src/cpu/instruction/opcode/sl/ShiftRightHLResetMSB.h
@@ -0,0 +1,19 @@
+#ifndef __SHIFT_RIGHT_HL_RESET_MSB_H__
+#define __SHIFT_RIGHT_HL_RESET_MSB_H__
+#include "../Instruction.h"
+
+class ShiftRightHLResetMSB : public Instruction{
+    private:
+        RegisterName registerName;
+    public:
+        ShiftRightHLResetMSB();
+
+        void execute(Memory&, Registers&) override;
+        unsigned int getSize() override;
+        unsigned int getTiming() override;
+        void setParameters(const Memory&, unsigned short) override;
+        std::string toString() override;
+};
+
+
+#endif
---
+++ src/cpu/instruction/opcode/sl/ShiftRightRegister.h
@@ -0,0 +1,19 @@
+#ifndef __SHIFT_RIGHT_REGISTER_H__
+#define __SHIFT_RIGHT_REGISTER_H__
+#include "../Instruction.h"
+
+class ShiftRightRegister : public Instruction{
+    private:
+        RegisterName registerName;
+    public:
+        ShiftRightRegister(RegisterName);
+
+        void execute(Memory&, Registers&) override;
+        unsigned int getSize() override;
+        unsigned int getTiming() override;
+        void setParameters(const Memory&, unsigned short) override;
+        std::string toString() override;
+};
+
+
+#endif
